d1c81d6195471129ab02863ae850b99667b48be5
Alexander Jacks <alexd.jacks@gmail.com>
2016-10-21 14:58:21 -0700

    fixes error where proj dir was nested inside extra dir

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>EmojiCal<i class="em em-clipboard"></i><i class="em em-cookie"></i><i class="em em-ok_hand"></i></h1>
+  `
+})
+
+export class AppComponent {
+
+}
