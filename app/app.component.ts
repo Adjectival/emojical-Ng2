aa96bbed458a7959d4626f2a314808652c2904db
Alexander Jacks <alexd.jacks@gmail.com>
2016-10-21 15:40:51 -0700

    adds click alert to test output binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,11 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>EmojiCal<i class="em em-clipboard"></i><i class="em em-cookie"></i><i class="em em-ok_hand"></i></h1>
-    <h2 *ngFor="let thisFood of foods">{{ thisFood.name }}</h2>
+    <div *ngFor="let thisFood of foods" (click)="reportMethod(thisFood)">
+      <h2>{{ thisFood.name }}</h2>
+      <h4>{{ thisFood.description }}</h4>
+      <h4 class="calNumber">{{ thisFood.calories }}</h4>
+    </div>
   </div>
   `
 })
@@ -16,6 +20,13 @@ export class AppComponent {
     new Food("12 oz vanilla sweet cream nitro cold brew", "actually coffee, not beer", 100),
     new Food("2pcs grilled fatty tuna nigiri", "tastes like charred", 240)
   ];
+  reportMethod(clickedFood: Food) {
+    if(clickedFood.calories >= 500) {
+      alert("Uh oh, over 500");
+    } else {
+      alert("Good job, you are what you eat!");
+    }
+  }
 }
 
 export class Food {
