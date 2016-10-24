c8d5c068db5a153c26ac840c824655414a770323
Alexander Jacks <alexd.jacks@gmail.com>
2016-10-23 21:48:12 -0700

    fixes edit functionality and moves Finish Edit button into edit interface; edit interface shows/hides on button clicks

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,16 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>EmojiCal<i class="em em-clipboard"></i><i class="em em-cookie"></i><i class="em em-ok_hand"></i></h1>
-    <div *ngFor="let selectedFood of foods" (click)="editFood(selectedFood)">
+    <div *ngFor="let selectedFood of foods">
       <h2>{{ selectedFood.name }}</h2>
       <h4>{{ selectedFood.description }}</h4>
       <h4 class="calNumber">{{ selectedFood.calories }}</h4>
+      <div>
+        <button (click)="editFood(selectedFood)" class="btn btn-lg btn-info">Edit this Food</button>
+      </div>
     </div>
-    <div class="well">
-      <h3>Click a Food Item above to Edit it</h3>
+    <div *ngIf="selectedFood" class="well" width="70%">
+      <h3>You sure you aren't fibbing?</h3>
       <div>
         <label>Food Name:</label>
         <input [(ngModel)]="selectedFood.name">
@@ -24,6 +27,9 @@ import { Component } from '@angular/core';
         <label>Food Calories:</label>
         <input [(ngModel)]="selectedFood.calories">
       </div>
+      <div>
+        <button (click)="endEditFood(selectedFood)" class ="btn btn-lg btn-primary">OK, Save Edit</button>
+      </div>
     </div>
   </div>
   `
@@ -35,9 +41,12 @@ export class AppComponent {
     new Food("12 oz vanilla sweet cream nitro cold brew", "actually coffee, not beer", 100),
     new Food("2pcs grilled fatty tuna nigiri", "tastes like charred", 240)
   ];
-  selectedFood: Food = this.foods[0];
+  selectedFood: Food = null;
   editFood(clickedFood: Food){
     this.selectedFood = clickedFood;
+  }
+  endEditFood(){
+    this.selectedFood = null;
   }
 }
 
